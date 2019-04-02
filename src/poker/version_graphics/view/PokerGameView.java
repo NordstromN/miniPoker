ed4992dac7441feb7db6fd84144487a79228d115
NordstromN <niklas.nordstroem@students.fhnw.ch>
2019-04-02 17:51:43 +0200

    Menubar nei in Game View, löschen separater Klasse

--- src/poker/version_graphics/view/PokerGameView.java
+++ src/poker/version_graphics/view/PokerGameView.java
@@ -1,7 +1,12 @@
 package poker.version_graphics.view;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,12 +16,35 @@ import poker.version_graphics.model.PokerGameModel;
 public class PokerGameView {
 	private VBox players;
 	private ControlArea controls;
-	private MenBar menuBar;
 	private PokerGameModel model;
+	private MenuBar menuBar;
+	private Menu game, settings;
+	private MenuItem changPl, statGameItem, closeGameItem, changeColourItem;
 	
 	public PokerGameView(Stage stage, PokerGameModel model) {
 		this.model = model;
 		
+		//Create and add MenuBar and first Menu Point
+		menuBar = new MenuBar();
+		game = new Menu("Game");
+		//Change nr of player in the game
+		changPl = new MenuItem("Change Player");
+		//Show Statistics of the game		
+		statGameItem = new MenuItem("Statistics");
+		//Leave the game
+		closeGameItem = new MenuItem("Close Game");
+		closeGameItem.setOnAction((ActionEvent e) -> Platform.exit());
+		menuBar.getMenus().add(game);
+		game.getItems().addAll(changPl, statGameItem, closeGameItem);
+		
+		//Add another Menu for Settings
+		settings = new Menu("Settings");
+		changeColourItem = new MenuItem("Change Colour");
+		//hangeColourItem.setOnAction((ActionEvent e) -> lülülülülü);
+		menuBar.getMenus().add(settings);
+		settings.getItems().add(changeColourItem);
+
+		//this.getMenus().addAll(game,settings);
 		
 		// Create all of the player panes we need, and put them into an HBox
 		players = new VBox();
@@ -25,21 +53,15 @@ public class PokerGameView {
 			pp.setPlayer(model.getPlayer(i)); // link to player object in the logic
 			players.getChildren().add(pp);
 		}
-		
-		
+			
 		// Create the control area
 		controls = new ControlArea();
 		controls.linkDeck(model.getDeck()); // link DeckLabel to DeckOfCards in the logic
-	
 		
-		//create the MenuBar
-		menuBar = new MenBar();
-
 		// Create root and set size
 		BorderPane root = new BorderPane();
 	
-		
-		// Put menu, players and controls into a BorderPane
+			// Put menu, players and controls into a BorderPane
 		root.setTop(menuBar);
 		root.setCenter(players);
 		root.setBottom(controls);
@@ -63,6 +85,11 @@ public class PokerGameView {
 		return (PlayerPane) players.getChildren().get(i);
 	}
 	
+	public MenuItem getChangPl() {
+		return this.changPl;
+	}
+	
+	
 	public Button getShuffleButton() {
 		return controls.btnShuffle;
 	}
@@ -75,5 +102,11 @@ public class PokerGameView {
 		return controls.btnShufDea;
 	}
 	
+	public void newGame() {
+		
+		
+		
+		
+	}
 	}
 	
